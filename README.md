9489c80a008219da020810c80cd33b53786c9abd
homepy <hopex40@163.com>
2019-03-31 15:38:57 +0800

    提交所有旧代码

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # meow-storm
+[![Build Status](https://travis-ci.org/homepy/meow-storm.svg?branch=master)](https://travis-ci.org/homepy/meow-storm)
+
+[![codecov](https://codecov.io/gh/homepy/meow-storm/branch/master/graph/badge.svg)](https://codecov.io/gh/homepy/meow-storm)
---
+++ src/main/java/io/github/homepy/meow/storm/topo/WordCountTopology.java
@@ -0,0 +1,47 @@
+package io.github.homepy.meow.storm.topo;
+
+import org.apache.storm.Config;
+import org.apache.storm.LocalCluster;
+import org.apache.storm.topology.TopologyBuilder;
+import org.apache.storm.tuple.Fields;
+import org.apache.storm.utils.Utils;
+
+import io.github.homepy.meow.storm.wordcount.ReportBolt;
+import io.github.homepy.meow.storm.wordcount.SentenceSpout;
+import io.github.homepy.meow.storm.wordcount.SplitSentenceBolt;
+import io.github.homepy.meow.storm.wordcount.WordCountBolt;
+
+public class WordCountTopology {
+	
+	private static final String SENTENCE_SPOUT_ID = "sentence-spout";
+	private static final String SPLIT_BOLT_ID = "split-bolt";
+	private static final String COUNT_BOLT_ID = "count-bolt";
+	private static final String REPORT_BOLT_ID = "report-bolt";
+	private static final String TOPOLOGY_NAME = "word-count-topology";
+
+	public static void main(String[] args) throws Exception {
+		SentenceSpout spout = new SentenceSpout();
+		SplitSentenceBolt splitBolt = new SplitSentenceBolt();
+		WordCountBolt countBolt = new WordCountBolt();
+		ReportBolt reportBolt = new ReportBolt();
+
+		TopologyBuilder builder = new TopologyBuilder();
+		builder.setSpout(SENTENCE_SPOUT_ID, spout);
+		// SentenceSpout --> SplitSentenceBolt 随机均匀分发
+		builder.setBolt(SPLIT_BOLT_ID, splitBolt, 2).setNumTasks(4).localOrShuffleGrouping(SENTENCE_SPOUT_ID);
+		// SplitSentenceBolt --> WordCountBolt 按"word"字段值分组分发
+		builder.setBolt(COUNT_BOLT_ID, countBolt, 4).fieldsGrouping(SPLIT_BOLT_ID, new Fields("word"));
+		// WordCountBolt --> ReportBolt 分发至唯一ReportBolt
+		builder.setBolt(REPORT_BOLT_ID, reportBolt).globalGrouping(COUNT_BOLT_ID);
+
+		Config config = new Config();
+		config.setNumWorkers(2);
+		
+		LocalCluster cluster = new LocalCluster();
+
+		cluster.submitTopology(TOPOLOGY_NAME, config, builder.createTopology());
+		Utils.sleep(5000L);
+		cluster.killTopology(TOPOLOGY_NAME);
+		cluster.shutdown();
+	}
+}
